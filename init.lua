565132c7606999dfcfb1dc1e3d82fa5459559f1b
Sergey Grigoriev <s.a.grigoriev@gmail.com>
2026-05-13 00:59:41 +0300

    Use cyberdream theme

--- init.lua
+++ init.lua
@@ -243,6 +243,22 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  { -- Cyberdream theme
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('cyberdream').setup {
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      }
+      vim.cmd('colorscheme cyberdream')
+    end,
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
